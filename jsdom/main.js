ed04e775bbb9a14d72c6302c5b86a0dd3fb8ba1d
Larissa Bragança <126626998+larissabraganca@users.noreply.github.com>
2023-08-07 20:31:16 -0300

    Add files via upload

---
+++ jsdom/main.js
@@ -0,0 +1,27 @@
+const ferro = document.querySelector("#ferro");
+
+ferro.addEventListener("click", function () {
+    alert('Eu sou o homem de ferro')
+})
+
+    function clicar() {
+        alert('Vamos montar o seu robô?')
+    }
+
+    clicar();
+
+
+
+    /*
+    font-size: 2em;
+    text-transform: uppercase;
+    padding: 0.2em 1em;
+    border: 3px solid var(--main-branco);
+    background: var(--main-branco);
+    clip-path: polygon(100% 0, 100% calc(100% - 30px), calc(100% - 30px) 100%, 0 100%, 0 0);
+    cursor: pointer;
+
+
+
+
+    */
